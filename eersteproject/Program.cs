2bf9467ee328fe888847bd640fbfed2550c4eb77
Akira <caseyegging2009@gmail.com>
2025-05-14 14:47:27 +0200

    commit

---
+++ eersteproject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        int episode = 3;
+        string episodeName = "Jinx"
+        bool happyEnding = false;
+        float rating = 9.7f;
+
+        Console.WriteLine("Episode: " + episode);
+        Console.WriteLine("Episode Name: " + episodeName);
+        Console.WriteLine("Happy Ending: " + happyEnding);
+        Console.WriteLine("Rating: " + rating);
+    }
+}
